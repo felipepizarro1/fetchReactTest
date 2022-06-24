135b75b59b233a1672679062d7f0951b77e86eae
Felipe <fpizarro.ux@gmail.com>
2022-06-24 17:04:15 +0200

    fetchupdate

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,1 +1,43 @@
-console.log(fetch('http://localhost:8080/pizza/api/pizzaSelection'));
+class PizzaAPI {
+
+    static list(callback) {
+        fetch("http://localhost:8080/administration/api/pizza")
+            .then(response => response.json())
+            .then(data => callback(data));
+    }
+
+    static load(name, image, ingredients, price, callback) {
+        const dto = {
+            name: name,
+            image: image,
+            ingredients: ingredients,
+            price: price
+        };
+        const url = "http://localhost:8080/administration/api/addPizza";
+
+        const requestOptions = {
+            method: 'POST',
+            'Content-Type': 'application/json',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dto)
+        };
+        fetch(url, requestOptions)
+            .then(response => callback());
+    }
+
+    static delete(dto, callback) {
+        const url = "http://localhost:8080/administration/api/deletePizza";
+
+        const requestOptions = {
+            method: 'POST',
+            'Content-Type': 'application/json',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(dto)
+        };
+        fetch(url, requestOptions)
+            .then(response => callback());
+    }
+
+}
+
+export default PizzaAPI;
